ef2856399dd73d13d3133e24b7ce0c8a3dd7ce71
Soren I. Bjornstad <contact@sorenbjornstad.com>
2020-10-31 19:12:52 -0500

    implement folding

--- main.lua
+++ main.lua
@@ -3,6 +3,35 @@ local inspect = require 'inspect'  -- DEBUG
 local lut_mod = require 'lut'
 local tersen_mod = require 'tersen'
 local trace_mod = require 'trace'
+local util = require 'util'
+
+
+local function refold(str, cols)
+    local lines = {}
+
+    for paragraph in str:gmatch("(.-)\n\n+") do
+        local cur_line = {}
+        local at_char = 0
+
+        for _, word in ipairs(util.split_whitespace(paragraph)) do
+            --TODO: super-long word handling
+            if at_char + #word + 1 > cols then
+                table.insert(lines, table.concat(cur_line, " "))
+                cur_line = {}
+
+                table.insert(cur_line, word)
+                at_char = #word
+            else
+                table.insert(cur_line, word)
+                at_char = at_char + #word + 1
+            end
+        end
+
+        table.insert(lines, table.concat(cur_line, " "))
+        table.insert(lines, "")
+    end
+    return table.concat(lines, "\n")
+end
 
 
 -- Get an operation callable that takes (lut, text).
@@ -23,17 +52,26 @@ local function get_parser()
         description = "tersen - the fast, flexible abbreviation engine",
         epilog = "Soren Bjornstad",
     }
-    parser:argument("tersen_dict",
-                    "Path to the tersen dictionary containing abbreviation mappings.")
-    parser:argument("files_to_tersen",
-                    "Text file(s) to be tersened; use - for stdin."
-                ):args("+")
-    parser:flag("-f --frequency",
-                "Instead of tersening input, analyze what would have been tersened "
-                .. "and print out the most frequently used words that don't have "
-                .. "abbreviations. ")
-    parser:flag("-o --at-once",
-                "Instead of reading a line at a time, read and process all input in a chunk.")
+    parser:argument(
+        "tersen_dict",
+        "Path to the tersen dictionary containing abbreviation mappings.")
+    parser:argument(
+        "files_to_tersen",
+        "Text file(s) to be tersened; use - for stdin.")
+        :args("+")
+    parser:flag(
+        "-w --width",
+        "Number of columns to re-fold the result to. Only useful with -o.")
+        :args("1")
+        :convert(tonumber)
+    parser:flag(
+        "-f --frequency",
+        "Instead of tersening input, analyze what would have been tersened "
+        .. "and print out the most frequently used words that don't have "
+        .. "abbreviations. ")
+    parser:flag(
+        "-o --at-once",
+        "Instead of reading a line at a time, read and process all input in a chunk.")
     return parser
 end
 
@@ -53,7 +91,11 @@ for _, v in ipairs(args.files_to_tersen) do
     if args.at_once then
         local text = f:read("*a")
         local res = get_callable(args)(lut, text)
-        print(res)
+        if args.width ~= nil then
+            print(refold(res, args.width))
+        else
+            print(res)
+        end
     else
         for line in f:lines() do
             local res = get_callable(args)(lut, line)
